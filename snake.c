45b5853df159faf8b766ceb8cc92f3660abb2425
Chen, Xinghong <xinghong.chen@intel.com>
2020-03-18 13:27:53 +0800

    refactor code

--- snake.c
+++ snake.c
@@ -5,20 +5,21 @@
 #include<termio.h>
 #include<pthread.h>
 #include<time.h>
+#include<assert.h>
 
 #define Width 65
 #define Heigh 25
+
 static struct termios new_settings,stored_settings;
 static int keyNumber;
 static pthread_mutex_t mutex;
-static pthread_mutex_t mutexsuspend;
-static pthread_cond_t cond;
-pthread_t inputpipe;
-pthread_t paintpipe;
-char pizz[Heigh][Width+1];
+static pthread_t keyboardlisten;
+static pthread_t workflow;
 
 static unsigned int speed = 1000000;
 int crush = 0;
+char pizz[Heigh][Width+1];
+
 int stridexmap[4] = {-1, 1, 0, 0};
 int strideymap[4] = {0, 0, -1, 1};
 
@@ -66,18 +67,21 @@ void printHead(){
 	return;
 }
 
-void *inputmodel(void *arg){
-	while(1){
-		int temp = getchar();
-		//printf("input chang to %d\n", temp);
-		pthread_mutex_lock(&mutex);
-		keyNumber = temp;
-		//if(keyNumber == 27){
-			//pthread_mutex_unlock(&mutex);
-			//break;
-		//}
-		pthread_mutex_unlock(&mutex);
-	}
+void designGame(){
+	printHead();
+	printf(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++  \n");
+	printf("                                                          \n");
+	printf("              welcome to the stupid game                   \n");
+	printf("                                                          \n");
+	printf(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++  \n");
+	printf(" Ps : esc key can help you exit, and left arrow , up arrow , down arrow, right arrow help you contral snake!\n");
+	printf(" please chose the diffcuty (easy / medium / hard / master) : ");
+	char diffcuty[50];
+	scanf("%s", diffcuty);
+	if (!strcmp(diffcuty, "easy")) speed = 1000000;
+	if (!strcmp(diffcuty, "medium")) speed = 500000;
+	if (!strcmp(diffcuty, "hard")) speed = 200000;
+	if (!strcmp(diffcuty, "master")) speed = 100000;
 }
 
 int genbean(){
@@ -98,94 +102,141 @@ int genbean(){
 		return 1;
 	}else{
 		pizz[randx][randy] = 'o';
-		return 0;
 	}
 	return 0;
 }
 
-void printsnake(){
-	snakebody* temp = head;
-	int tempx=0;
-	int tempy=0;
-	int tailx=0;
-	int taily=0;
-	int checktag = stridexmap[keyvalue]+head->stridex | strideymap[keyvalue] +head->stridey;
-	while(temp){
-		if (temp == head){
-			tempx = temp->stridex;
-			tempy = temp->stridey;
-			if (!checktag){
-				temp->x += temp->stridex;
-				temp->y += temp->stridey;
-			}else{
-				temp->stridex = stridexmap[keyvalue];
-				temp->stridey = strideymap[keyvalue];
-				temp->x += temp->stridex;
-				temp->y += temp->stridey;
-			}
-			temp = temp->next;
-			continue;
-		}
-		int bodystridex = temp->stridex;
-		int bodystridey = temp->stridey;
-		temp->stridex = tempx;
-		temp->stridey = tempy;
-		if(!temp->next){
-			pizz[temp->x][temp->y] = ' ';
-			tailx = temp->x;
-			taily = temp->y;
-		}
-		temp->x += temp->stridex;
-		temp->y += temp->stridey;
-		tempx = bodystridex;
-		tempy = bodystridey;
-		temp = temp->next;
+int crushonwall(){
+	int x = head->x;
+	int y = head->y;
+	if(x==0 || x== Heigh-1 || y == 0 || y == Width-1){
+		crush = 1;
+		return crush;
 	}
+	return 0;
+}
 
+int crushonbean(int x, int y, int stridex, int stridey){
 	if (pizz[head->x][head->y] == 'o'){
 		snakebody *tail = (snakebody*)malloc(sizeof(snakebody));
-		tail->x = tailx;
-		tail->y = taily;
-		tail->stridex = tempx;
-		tail->stridey = tempy;
+		tail->x = x;
+		tail->y = y;
+		tail->stridex = stridex;
+		tail->stridey = stridey;
 		tail->next = NULL;
-		temp = head;
-		while(temp->next) {temp = temp->next;}
-		temp->next = tail;
+		snakebody *cursor = head;
+		while(cursor->next) {cursor = cursor->next;}
+		cursor->next = tail;
 		while(genbean()){}
-	}else if (pizz[head->x][head->y]!=' '){
-		crush = 1;
+		return 1;
 	}
+	return 0;
+}
 
-	temp = head->next;
-	while(temp){
-		if (temp->x == head->x && temp->y == head->y){
-                	crush = 1;
+int crushonbody(){
+	snakebody *cursor = head->next;
+	while(cursor){
+		if (cursor->x == head->x && cursor->y == head->y){
+            crush = 1;
 			break;
 		}
-		temp = temp->next;
+		cursor = cursor->next;
 	}
-	
+	return crush;
+}
+
+void handlecrush(){
 	if (crush){
 		char *endlog = "                 your  are   loser !!!!!!!!           ";
 		memcpy(pizz[Heigh/2], endlog, strlen(endlog));
 	}else{
-        	temp = head;
-		while(temp){
-                	pizz[temp->x][temp->y] = 'O';
-                	temp = temp->next;
-        	}
+        snakebody *cursor = head;
+		while(cursor){
+			pizz[cursor->x][cursor->y] = 'O';
+			cursor = cursor->next;
+        }
 	}
+}
+
+void printsnake(){
+	snakebody* cursor = head;
+	int newstridex=0;
+	int newstridey=0;
+	int tailx=0;
+	int taily=0;
+	int inputvalidate = stridexmap[keyvalue]+head->stridex | strideymap[keyvalue] +head->stridey;
+	while(cursor){
+		if (cursor == head){
+			newstridex = cursor->stridex;
+			newstridey = cursor->stridey;
+			if (!inputvalidate){
+				cursor->x += cursor->stridex;
+				cursor->y += cursor->stridey;
+			}else{
+				cursor->stridex = stridexmap[keyvalue];
+				cursor->stridey = strideymap[keyvalue];
+				cursor->x += cursor->stridex;
+				cursor->y += cursor->stridey;
+			}
+			cursor = cursor->next;
+			continue;
+		}
+		int bodystridex = cursor->stridex;
+		int bodystridey = cursor->stridey;
+		cursor->stridex = newstridex;
+		cursor->stridey = newstridey;
+		if(!cursor->next){
+			pizz[cursor->x][cursor->y] = ' ';
+			tailx = cursor->x;
+			taily = cursor->y;
+		}
+		cursor->x += cursor->stridex;
+		cursor->y += cursor->stridey;
+		newstridex = bodystridex;
+		newstridey = bodystridey;
+		cursor = cursor->next;
+	}
+	crushonbean(tailx, taily, newstridex, newstridey);
+	crushonwall();
+	crushonbody();
+	handlecrush();
 	for(int i=0; i<Heigh; i++){
 		printf("%s\n",pizz[i]);
 	}
+}
 
+void initsnake(){
+	keyNumber = 0;
+	keyvalue = left;	
+	int startx = Heigh/2;
+	int starty = Width/2;
+	snakebody* temp = head;
+	for (int i=0; i<5; i++){
+		snakebody* body = (snakebody*)malloc(sizeof(snakebody));
+		body->x = startx;
+		body->y = starty+i;
+		body->stridex = 0;
+		body->stridey = -1;
+		body->next = NULL;
+		if(!head){
+			head = body;
+			temp = body;
+		}else{
+			temp->next = body;
+			temp = body;
+		}
+	}
+	temp = head;
+	while(temp){
+		int x = temp->x;
+		int y = temp->y;
+		pizz[x][y] = 'O';
+		temp = temp->next;		
+	}
 }
 
 void initpizz(){
 	initkeyboard();
-	keyNumber = 0;
-	keyvalue = left;
 	for(int i=0;i<Heigh;i++){
 		for(int j=0;j<=Width;j++){
 			if(j==Width){
@@ -201,45 +252,19 @@ void initpizz(){
 			}	
 		}
 	}
-	int startx = Heigh/2;
-	int starty = Width/2;
-	head = (snakebody *)malloc(sizeof(snakebody));
-	head->x = startx;
-	head->y = starty;
-	head->stridex = 0;
-	head->stridey = -1;
-	head->next = NULL;
-	snakebody* temp = head;
-	for (int i=1; i<5; i++){
-		snakebody* body = (snakebody*)malloc(sizeof(snakebody));
-		body->x = startx;
-		body->y = starty+i;
-		body->stridex = 0;
-		body->stridey = -1;
-		body->next = NULL;
-		temp->next = body;
-		temp = body;
-	}
-	temp = head;
-	for(int i=0; i<5; i++){
-		int x = temp->x;
-		int y = temp->y;
-		pizz[x][y] = 'O';
-		temp = temp->next;
-	}
+	initsnake();
 	while(genbean()){}
 }
 
-void *paint(void *arg){
-	//char pizz[Width][Heigh];
+void *Controlflow(void *arg){
 	initpizz();
 	while(1){
 		pthread_mutex_lock(&mutex);
 		printHead();
 		if(keyNumber == 27){
-                        pthread_mutex_unlock(&mutex);
-                        break;
-                }
+			pthread_mutex_unlock(&mutex);
+			break;
+        }
 		switch(keyNumber){
 			case 65:
 				keyvalue = up;
@@ -265,69 +290,63 @@ void *paint(void *arg){
 	}
 }
 
-int main(int argv, char *argc[]){
-	int ret;
-	pthread_mutex_init(&mutex, NULL);
-	printHead();
-	printf(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++  \n");
-	printf("                                                          \n");
-	printf("              welcome to the stupid game                   \n");
-	printf("                                                          \n");
-	printf(" +++++++++++++++++++++++++++++++++++++++++++++++++++++++  \n");
-	printf(" Ps : esc key can help you exit, and left arrow , up arrow , down arrow, right arrow help you contral snake!\n");
-	printf(" please chose the diffcuty (easy / medium / hard / master) : ");
-	char diffcuty[50];
-	scanf("%s", diffcuty);
-	if (!strcmp(diffcuty, "easy")) speed = 1000000;
-	if (!strcmp(diffcuty, "medium")) speed = 500000;
-	if (!strcmp(diffcuty, "hard")) speed = 200000;
-	if (!strcmp(diffcuty, "master")) speed = 100000;
-	while(!crush){
-		if(ret = pthread_create(&inputpipe, NULL, inputmodel, NULL)){
-			printf("creat inputpipe thread failed, %s\n", strerror(ret));
-			return ret;
-		}
-		if(ret = pthread_create(&paintpipe, NULL, paint, NULL)){
-			printf("creat painpipe thread failed, %s\n", strerror(ret));
-			return ret;
-		}
-		pthread_join(paintpipe, NULL);
-		pthread_cancel(inputpipe);
-		if(!crush){
-			break;
-		}
-		int record = 0;
-		snakebody *temp;
-		while(head){
-			temp = head->next;
-			free(head);
-			head=temp;
-			record++;
-		}
-		printf("your record is %d\n", record);
-		while(crush){
-			char yorn;
-                	printf("you lose, please chose if you want start again(y or n) :");
-			scanf("%c", &yorn);
-                	if (yorn == 'y'){
-                		crush = 0;
-                        	break;
-               		}else if(yorn == 'n'){
-                        	break;
-                	}else{
-                		printf("please input legal charactor !\n");
-                        	continue;
-                	}
-		}
-
-	}
+int freesnake(){
+	int record = 0;
 	snakebody *temp;
 	while(head){
 		temp = head->next;
 		free(head);
 		head = temp;
-		
+		record++;
 	}
+	return record;
+}
+
+void *startlistenkeyboard(void *arg){
+	while(1){
+		int temp = getchar();
+		pthread_mutex_lock(&mutex);
+		keyNumber = temp;
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+
+int main(int argv, char *argc[]){
+	pthread_mutex_init(&mutex, NULL);
+	designGame();
+	while(!crush){
+		int ret = 0;		
+		if(ret = pthread_create(&keyboardlisten, NULL, startlistenkeyboard, NULL)){
+			printf("creat inputpipe thread failed, %s\n", strerror(ret));
+			return ret;
+		}
+		if(ret = pthread_create(&workflow, NULL, Controlflow, NULL)){
+			printf("creat painpipe thread failed, %s\n", strerror(ret));
+			return ret;
+		}
+		pthread_join(workflow, NULL);
+		pthread_cancel(keyboardlisten);
+		if(!crush){
+			break;
+		}
+		printf("your record is %d\n", freesnake());
+		while(crush){
+			char yorn;
+            printf("you lose, please chose if you want start again(y or n) :");
+			scanf("%c", &yorn);
+			if (yorn == 'y'){
+				crush = 0;
+				break;
+			}else if(yorn == 'n'){
+				break;
+			}else{
+				printf("please input legal charactor !\n");
+				continue;
+			}
+		}
+	}
+	freesnake();
 	closekeyboard();
 
 	return 0;
